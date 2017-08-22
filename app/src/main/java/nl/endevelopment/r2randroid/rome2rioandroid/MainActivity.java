a1bcd1267a279f7162d1bb9db2117450472d2e13
Jan van den Ende <janvandenende@gmail.com>
2017-08-22 15:23:13 +0200

    library upgrades

---
+++ app/src/main/java/nl/endevelopment/r2randroid/rome2rioandroid/MainActivity.java
@@ -0,0 +1,16 @@
+package nl.endevelopment.r2randroid.rome2rioandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+    }
+}
